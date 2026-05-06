9d95850f2954912f4e3f416e6e3a58e2feed3a0e
Tibbe <tlin.jonas@gmail.com>
2026-05-06 12:24:08 +0200

    get running Timer from server useEffect

--- src/components/Stopwatch.tsx
+++ src/components/Stopwatch.tsx
@@ -1,29 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import { useStopwatch } from 'react-timer-hook';
-import type { Timer, User } from '../types/types';
-import useTimerStatus from '../hooks/useTimerStatus';
+import React, { useEffect, useState } from "react";
+import { useStopwatch } from "react-timer-hook";
+import type { Timer, User } from "../types/types";
+import useTimerStatus from "../hooks/useTimerStatus";
 
 //https://www.npmjs.com/package/react-timer-hook
 
 interface StopwatchProps {
-  setCurrentTime: number
-  currentUser: string
+  setCurrentTime: (time: number) => void;
+  currentUserId: string | null;
 }
 
-function Stopwatch({setCurrentTime, currentUser} : StopwatchProps) {
-  const [currentStopwatch, setCurrentStopwatch] = useState<number>(0)
-  useEffect (() => {
-    setCurrentTime = currentStopwatch
-}, [currentStopwatch])
+function Stopwatch({ setCurrentTime, currentUserId }: StopwatchProps) {
+  const [currentStopwatch, setCurrentStopwatch] = useState<number>(0);
+  const [autoStart, setAutoStart] = useState<boolean>(false);
+  const [offsetTime, setOffsetTime] = useState<Date>();
+  const checkTimer: Timer | null = useTimerStatus(currentUserId ?? undefined);
+  useEffect(() => {
+    setCurrentTime(currentStopwatch);
+  }, [currentStopwatch]);
 
-useEffect (() => {
-  const checkTimer: Timer | null = useTimerStatus()
-  if (checkTimer) {
-    const offsetTime = checkTimer.duration
-    console.log(offsetTime);
-    
-  }
-}, [])
+  //Checks if there's a timer that was never stopped
+  useEffect(() => {
+    if (checkTimer) {
+      const status = checkTimer.status;
+      if (status == "STARTED") {
+        const timerTime:number = new Date().getTime() - checkTimer.timeStart.getTime();
+        const newOffset:Date = new Date()
+        newOffset.setMilliseconds(newOffset.getMilliseconds() + timerTime)
+        setOffsetTime(newOffset)
+        setAutoStart(true);
+      } else {
+        setAutoStart(false);
+        const offsetTime = checkTimer.duration;
+      }
+      console.log(offsetTime);
+    }
+  }, []);
 
   const {
     totalSeconds,
@@ -36,20 +48,17 @@ useEffect (() => {
     start,
     pause,
     reset,
-  } = useStopwatch({ autoStart: false, interval: 90});
-  
-
+  } = useStopwatch({ autoStart, interval: 90, offsetTimestamp:  });
 
   return (
     <div>
       <p>Stopwatch</p>
-      <div style={{fontSize: '35px'}}>
-        <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>:<span>{milliseconds}</span>
+      <div style={{ fontSize: "35px" }}>
+        <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:
+        <span>{seconds}</span>:<span>{milliseconds}</span>
       </div>
-      <div>
-
-      </div>
-      <p>{isRunning ? 'Running' : 'Not running'}</p>
+      <div></div>
+      <p>{isRunning ? "Running" : "Not running"}</p>
       <button onClick={start}>Start</button>
       <button onClick={pause}>Pause</button>
       <button onClick={() => reset()}>Reset</button>
@@ -57,4 +66,4 @@ useEffect (() => {
   );
 }
 
-export default Stopwatch
+export default Stopwatch;
