edfa7cd8a280374db81f74b6fed363822b99fd8e
Tibbe <tlin.jonas@gmail.com>
2026-05-10 01:07:00 +0200

    updated url to production

--- src/data.tsx
+++ src/data.tsx
@@ -1,6 +1,6 @@
 // Local url
- export const url = `http://localhost:8080/`
+// export const url = `http://localhost:8080/`
 
 //Production url
-// export const url = `https://seashell-backend-m5vt7.ondigitalocean.app/`
+export const url = `https://seashell-backend-m5vt7.ondigitalocean.app/`
 export default url
